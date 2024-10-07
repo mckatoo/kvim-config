bf0e3706e54d5942b0da2511336f62f236205267
Milton Carlos Katoo <mckatoo@gmail.com>
2024-10-07 19:07:04 -0300

    change from vim commands to vim options

--- init.lua
+++ init.lua
@@ -1,8 +1,21 @@
 ---@diagnostic disable: undefined-global
 local vim = vim
 vim.opt.clipboard = 'unnamedplus'
-vim.cmd([[ set rtp += "~/.config/kvim" ]])
+-- vim.cmd([[ set rtp += "~/.config/kvim" ]])
+-- vim.opt.rtp += "~/.config/kvim"
 require("config.lazy")
 require("config.keys")
-vim.cmd([[ set number ]])
-vim.cmd([[ set nowrap ]])
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+
+vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
+vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.wrap = true -- wrap lines
+
+-- use treesitter folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
